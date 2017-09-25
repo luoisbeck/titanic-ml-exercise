56b9c1b8b68398b2cfd7bdac70c2289ac72c0dfc
luoisbeck <lpgoncalo@gmail.com>
2017-09-25 23:24:51 +0100

    changed csv parser to Pandas from Numpy

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-import numpy as np
+# import numpy as np
+import pandas as pd
 # from sklearn.model_selection import train_test_split
 
-hist_data = np.genfromtxt('train.csv', delimiter=',', dtype=None)
-# PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+# changed to pandas due to performance
+hist_data =pd.read_csv('train.csv', sep=',', header=None)
+# HEADERS: PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 
-print(hist_data[0])
+print("First row Name: " + str(hist_data.values[0][3]))
+
